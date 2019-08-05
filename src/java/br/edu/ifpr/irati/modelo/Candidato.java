8457200308c2429a200827a0e4a59b608e9682f3
B61 <B61@10.33.0.140>
2019-08-05 10:53:00 -0300

    Revert "Commmit das classes renomeadas. Com mínimas modificações."
    This reverts commit dedd81b142ebcb38741743ede2b29821f2a7001e.

--- src/java/br/edu/ifpr/irati/modelo/Candidato.java
+++ src/java/br/edu/ifpr/irati/modelo/Candidato.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package br.edu.ifpr.irati.modelo;
+
 import java.io.Serializable;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.PrimaryKeyJoinColumn;
